70224eb760d565d9ae073566783a4e2daa35be35
Friedrich482 <friedricht109@gmail.com>
2024-05-17 21:16:02 +0200

    feat: popular manga
    successfully get english title from the response even if the title is undefined (using altTitles array)

--- components/HomePage/MostPopular.tsx
+++ components/HomePage/MostPopular.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import newMangaDex from "@/fetch-functions/fetchPopular";
 
-import { IMangaResult } from "@consumet/extensions";
-
 const MostPopular = async () => {
   const mangaDex = new newMangaDex();
   const res = (await mangaDex.fetchPopular(1, 10)).results;
@@ -11,33 +9,55 @@ const MostPopular = async () => {
     async (result) => await mangaDex.fetchMangaInfo(result.id),
   );
   const popularMangaS = await Promise.all(popularMangaPromises);
-  console.log(popularMangaS[6]);
   return (
     <section className="flex w-4/12 flex-col items-center justify-center gap-5 place-self-end">
-      {popularMangaS.map((popularManga) => (
-        <div key={popularManga.id} className="flex items-center justify-center">
-          <Image
-            className="w-20"
-            priority={false}
-            alt={popularManga.title as string}
-            src={popularManga.image as string}
-            width={300}
-            height={300}
-          />
-          <div className="flex w-4/5 flex-col items-center justify-center">
-            <div>
-              {popularManga.title
-                ? (popularManga.title as string)
-                : popularManga.altTitles
-                  ? typeof popularManga.altTitles === "string"
-                    ? popularManga.altTitles
-                    : popularManga.altTitles
-                  : ""}
+      {popularMangaS.map((popularManga) => {
+        let englishTitle: string | null = null;
+
+        if (popularManga.altTitles) {
+          if (typeof popularManga.altTitles === "string") {
+            englishTitle = popularManga.altTitles;
+          } else {
+            for (const mangaTitle of popularManga.altTitles) {
+              if (typeof mangaTitle === "string") {
+                englishTitle = mangaTitle;
+                break;
+              }
+              if (mangaTitle.hasOwnProperty("en")) {
+                englishTitle = mangaTitle["en"];
+                break;
+              }
+            }
+          }
+        }
+
+        return (
+          <div
+            key={popularManga.id}
+            className="flex items-center justify-center"
+          >
+            <Image
+              className="w-20"
+              priority={false}
+              alt={popularManga.title as string}
+              src={popularManga.image as string}
+              width={300}
+              height={300}
+            />
+            <div className="flex w-4/5 flex-col items-center justify-center">
+              <div>
+                {popularManga.title ? (
+                  <p>{popularManga.title as string}</p>
+                ) : englishTitle ? (
+                  <p>{englishTitle}</p>
+                ) : (
+                  <p>lol</p>
+                )}
+              </div>
             </div>
-            {/* <div>{popularManga. as string}</div> */}
           </div>
-        </div>
-      ))}
+        );
+      })}
     </section>
   );
 };
