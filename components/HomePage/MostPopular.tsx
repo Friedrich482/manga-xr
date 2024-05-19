e2e6d3bb3a6cecd0b37719c6a7874c0353c2aa60
Friedrich482 <friedricht109@gmail.com>
2024-05-19 23:55:35 +0200

    feat: mostPopular
    started the design of the most popular section (when the screen width is less than 860px)

--- components/HomePage/MostPopular.tsx
+++ components/HomePage/MostPopular.tsx
@@ -3,15 +3,29 @@ import { BsFire } from "react-icons/bs";
 import PopularMangaList from "./popularMangaList";
 const MostPopular = () => {
   return (
-    <section className="my-14 mr-10 flex w-1/4 min-w-60 max-w-80 flex-col items-center justify-center gap-8 place-self-end text-neutral-700 dark:text-neutral-300">
-      <h2 className="w-full text-xl">
-        <Link href={"/"} className="flex items-center justify-center gap-2">
-          <BsFire />
-          <span>Popular</span>
-        </Link>
-      </h2>
-      <PopularMangaList />
-    </section>
+    <>
+      {/* large screens (more than 860px)*/}
+      <section className="my-14 mr-10 hidden w-1/4 min-w-60 max-w-80 flex-col items-center justify-center gap-8 place-self-end text-neutral-700 dark:text-neutral-300 large-nav:flex">
+        <h2 className="mb-2 w-full text-3xl text-neutral-700 hover:text-default-black dark:text-neutral-300 dark:hover:text-default-white">
+          <Link href={"/"} className="flex items-center justify-center gap-2">
+            <BsFire />
+            <span>Popular</span>
+          </Link>
+        </h2>
+        <PopularMangaList />
+      </section>
+
+      {/*   smaller screens (less than 860px)*/}
+      <section className="flex flex-col large-nav:hidden">
+        <h2 className="mb-2 w-full text-3xl text-neutral-700 hover:text-default-black dark:text-neutral-300 dark:hover:text-default-white">
+          <Link href={"/"} className="flex items-center justify-center gap-2">
+            <BsFire />
+            <span>Popular now</span>
+          </Link>
+        </h2>
+        <PopularMangaList />
+      </section>
+    </>
   );
 };
 export default MostPopular;
--- components/HomePage/popularMangaList.tsx
+++ components/HomePage/popularMangaList.tsx
@@ -1,6 +1,6 @@
 import PopularMangaElement from "./popularMangaElement";
 import newMangaDex from "@/fetch-functions/fetchPopular";
-
+import { IMangaInfo } from "@consumet/extensions";
 const PopularMangaList = async () => {
   // data fetching: popularMangaS
 
@@ -11,57 +11,84 @@ const PopularMangaList = async () => {
   );
   const popularMangaS = await Promise.all(popularMangaPromises);
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-8">
-      {popularMangaS.map((popularManga) => {
-        let englishTitle: string | null = null;
-        if (popularManga.altTitles) {
-          if (typeof popularManga.altTitles === "string") {
-            englishTitle = popularManga.altTitles;
-          } else {
-            for (const mangaTitle of popularManga.altTitles) {
-              if (typeof mangaTitle === "string") {
-                englishTitle = mangaTitle;
-                break;
-              }
-              if (mangaTitle.hasOwnProperty("en")) {
-                englishTitle = mangaTitle["en"];
-                break;
-              }
-            }
+  const getMangaInfo = (popularManga: IMangaInfo) => {
+    let englishTitle: string | null = null;
+    if (popularManga.altTitles) {
+      if (typeof popularManga.altTitles === "string") {
+        englishTitle = popularManga.altTitles;
+      } else {
+        for (const mangaTitle of popularManga.altTitles) {
+          if (typeof mangaTitle === "string") {
+            englishTitle = mangaTitle;
+            break;
+          }
+          if (mangaTitle.hasOwnProperty("en")) {
+            englishTitle = mangaTitle["en"];
+            break;
           }
         }
+      }
+    }
 
-        // This variable allows slice when the title (or englishTitle) is too long
-        const lastCharacter = popularManga.title
-          ? (popularManga.title as string).indexOf(" - ") !== -1
-            ? (popularManga.title as string).indexOf(" - ")
-            : (popularManga.title as string).length - 1
-          : (englishTitle as string).indexOf(" - ") !== -1
-            ? (englishTitle as string).indexOf(" - ")
-            : (englishTitle as string).length - 1;
+    // This variable allows slice when the title (or englishTitle) is too long
+    const lastCharacter = popularManga.title
+      ? (popularManga.title as string).indexOf(" - ") !== -1
+        ? (popularManga.title as string).indexOf(" - ")
+        : (popularManga.title as string).length - 1
+      : (englishTitle as string).indexOf(" - ") !== -1
+        ? (englishTitle as string).indexOf(" - ")
+        : (englishTitle as string).length - 1;
 
-        //  get the last chapter
-        const lastChapter = popularManga.chapters
-          ? popularManga.chapters?.length >= 1
-            ? popularManga.chapters[0].chapterNumber
-            : ""
-          : "";
+    //  get the last chapter
+    const lastChapter = popularManga.chapters
+      ? popularManga.chapters?.length >= 1
+        ? popularManga.chapters[0].chapterNumber
+        : ""
+      : "";
 
-        // get the genres
-        const genres = popularManga.genres?.slice(0, 3);
-        return (
-          <PopularMangaElement
-            englishTitle={englishTitle}
-            lastCharacter={lastCharacter}
-            lastChapter={lastChapter}
-            popularManga={popularManga}
-            key={popularManga.id}
-            genres={genres}
-          />
-        );
-      })}
-    </div>
+    // get the genres
+    const genres = popularManga.genres?.slice(0, 3);
+    return { englishTitle, lastCharacter, lastChapter, genres };
+  };
+  return (
+    <>
+      {/* large screens (more than 860px)*/}
+      <div className="hidden flex-col items-center justify-center gap-8 large-nav:flex">
+        {popularMangaS.map((popularManga) => {
+          const { englishTitle, lastCharacter, lastChapter, genres } =
+            getMangaInfo(popularManga);
+          return (
+            <PopularMangaElement
+              englishTitle={englishTitle}
+              lastCharacter={lastCharacter}
+              lastChapter={lastChapter}
+              popularManga={popularManga}
+              key={popularManga.id}
+              genres={genres}
+            />
+          );
+        })}
+      </div>
+
+      {/*   smaller screens (less than 860px)*/}
+
+      <div className="popular flex h-[21.875rem] w-full overflow-x-scroll bg-white dark:bg-neutral-900 large-nav:hidden">
+        {popularMangaS.map((popularManga) => {
+          const { englishTitle, lastCharacter, lastChapter, genres } =
+            getMangaInfo(popularManga);
+          return (
+            <PopularMangaElement
+              englishTitle={englishTitle}
+              lastCharacter={lastCharacter}
+              lastChapter={lastChapter}
+              popularManga={popularManga}
+              key={popularManga.id}
+              genres={genres}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 export default PopularMangaList;
