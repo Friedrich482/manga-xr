c8a8aed5a922745104becc6043c9a3df83936882
Friedrich482 <friedricht109@gmail.com>
2024-06-09 02:44:59 +0200

    feat: last chapter
    got some last chapters but not every manga especially for the search results
    
    maybe the best idea is to try the documentation`feed` option to get the last chapter. Does it work in all cases ?

--- components/HomePage/MostPopular.tsx
+++ components/HomePage/MostPopular.tsx
@@ -8,7 +8,7 @@ const MostPopular = () => {
       <section className="my-14 mr-10 mt-20 hidden w-1/4 min-w-60 max-w-80 flex-col items-center justify-center gap-8 place-self-end self-start text-neutral-700 dark:text-neutral-300 large-nav:flex">
         <h2 className="mb-2 w-full text-3xl text-neutral-700 hover:text-default-black dark:text-neutral-300 dark:hover:text-default-white">
           <Link href={"/"} className="flex items-center justify-center gap-2">
-            <BsFire />
+            <BsFire className="text-orange-400" />
             <span>Popular</span>
           </Link>
         </h2>
@@ -19,7 +19,7 @@ const MostPopular = () => {
       <section className="overflow-x mt-16 flex w-full flex-col large-nav:hidden">
         <h2 className="mb-6 w-full text-3xl text-neutral-700 hover:text-default-black dark:text-neutral-300 dark:hover:text-default-white">
           <Link href={"/"} className="flex items-center justify-center gap-2">
-            <BsFire />
+            <BsFire className="text-orange-400" />
             <span>Popular now</span>
           </Link>
         </h2>
