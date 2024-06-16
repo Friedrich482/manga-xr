2ab1dc8ebfe12f2844e9ee474155917e0f1b065f
Friedrich482 <friedricht109@gmail.com>
2024-06-16 23:47:32 +0200

    refactor: navList
    refactored each navList using a map function and an array of elements

--- components/Navbar/HorizontalNavList.tsx
+++ components/Navbar/HorizontalNavList.tsx
@@ -5,70 +5,60 @@ import { BsFire } from "react-icons/bs";
 import { FaClipboardList } from "react-icons/fa";
 import { twMerge as tm } from "tailwind-merge";
 import { usePathname } from "next/navigation";
+export const elements = [
+  {
+    name: "Home",
+    icon: IoMdHome,
+    pathName: "/",
+    width: "w-3/12",
+  },
+  {
+    name: "Popular",
+    icon: BsFire,
+    pathName: "/popular",
+    width: "w-4/12",
+  },
+  {
+    name: "List",
+    icon: FaClipboardList,
+    pathName: "/list",
+    width: "w-3/12",
+  },
+];
 const HorizontalNavList = () => {
   const pathName = usePathname();
   return (
     <ul className="hidden w-5/12 flex-row items-center justify-start gap-5 large-nav:flex">
-      <li className="text-neutral flex w-3/12 items-center justify-self-center text-center text-xl ring-0">
-        <Link
-          href={"/"}
-          className="group flex w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 transition duration-500 ease-out hover:bg-neutral-300 hover:text-black dark:text-neutral-300 dark:hover:bg-neutral-800"
+      {elements.map((element) => (
+        <li
+          className={`text-neutral flex ${element.width} items-center justify-self-center text-center text-xl`}
+          key={element.name}
         >
-          <IoMdHome
+          <Link
+            href={element.pathName}
             className={tm(
-              "size-6 text-neutral-400 group-hover:font-bold",
-              pathName === "/" && "text-white",
-            )}
-          />
-          <span
-            className={tm("text-neutral-400", pathName === "/" && "text-white")}
-          >
-            Home
-          </span>
-        </Link>
-      </li>
-      <li className="text-neutral flex w-4/12 items-center justify-self-center text-center text-xl">
-        <Link
-          href={"/"}
-          className="group flex  w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 transition duration-500 ease-out hover:bg-neutral-300 hover:text-black dark:text-neutral-300  dark:hover:bg-neutral-800"
-        >
-          <BsFire
-            className={tm(
-              "size-5 flex-shrink-0 text-neutral-400",
-              pathName === "/popular" && "text-white",
-            )}
-          />
-          <span
-            className={tm(
-              "text-neutral-400",
-              pathName === "/popular" && "text-white",
+              "group flex w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 transition duration-500 ease-out hover:bg-neutral-300 hover:text-black dark:text-neutral-300 dark:hover:bg-neutral-800",
+              pathName === element.pathName &&
+                "border-b-[1px] border-r-[1px] border-b-violet-500 border-r-violet-500 hover:border-transparent",
             )}
           >
-            Popular
-          </span>
-        </Link>
-      </li>
-      <li className="text-neutral flex w-3/12 items-center justify-self-center text-center text-xl">
-        <Link
-          href={"/"}
-          className="group flex w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 transition duration-500 ease-out hover:bg-neutral-300 hover:text-black dark:text-neutral-300  dark:hover:bg-neutral-800"
-        >
-          <FaClipboardList
-            className={tm(
-              "size-5 text-neutral-400 group-hover:font-bold",
-              pathName === "/list" && "text-white",
-            )}
-          />
-          <span
-            className={tm(
-              "text-neutral-400",
-              pathName === "/list" && "text-white",
-            )}
-          >
-            List
-          </span>
-        </Link>
-      </li>
+            <element.icon
+              className={tm(
+                "size-5 flex-shrink-0 text-neutral-400",
+                pathName === element.pathName && "text-white",
+              )}
+            />
+            <span
+              className={tm(
+                "text-neutral-400",
+                pathName === element.pathName && "text-white",
+              )}
+            >
+              {element.name}
+            </span>
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
