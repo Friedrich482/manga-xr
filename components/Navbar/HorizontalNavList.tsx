7517e38ce572668e02ff4d91755975c62745a239
Friedrich482 <friedricht109@gmail.com>
2024-06-21 19:22:19 +0200

    refactor: navList
    refactored the navLists code and added a special style depending of the pathname

--- components/Navbar/HorizontalNavList.tsx
+++ components/Navbar/HorizontalNavList.tsx
@@ -1,74 +1,62 @@
 "use client";
 import Link from "next/link";
-import { IoMdHome } from "react-icons/io";
-import { BsFire } from "react-icons/bs";
-import { FaClipboardList } from "react-icons/fa";
+import { BsFire } from "react-Icons/bs";
 import { twMerge as tm } from "tailwind-merge";
 import { usePathname } from "next/navigation";
+import { IoMdHome } from "react-icons/io";
+import { FaClipboardList } from "react-icons/fa";
+export const links = [
+  { name: "Home", path: "/", width: "w-3/12", Icon: IoMdHome },
+  {
+    name: "Popular",
+    path: "/popular",
+    width: "w-4/12",
+    Icon: BsFire,
+  },
+  {
+    name: "List",
+    path: "/list",
+    width: "w-3/12",
+    Icon: FaClipboardList,
+  },
+];
 const HorizontalNavList = () => {
   const pathName = usePathname();
   return (
     <ul className="hidden w-5/12 flex-row items-center justify-start gap-5 large-nav:flex">
-      <li className="text-neutral flex w-3/12 items-center justify-self-center text-center text-xl ring-0">
-        <Link
-          href={"/"}
-          className="group flex w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 transition duration-500 ease-out hover:bg-neutral-300 hover:text-black dark:text-neutral-300 dark:hover:bg-neutral-800"
-        >
-          <IoMdHome
-            className={tm(
-              "size-6 text-neutral-400 group-hover:font-bold",
-              pathName === "/" && "text-white",
-            )}
-          />
-          <span
-            className={tm("text-neutral-400", pathName === "/" && "text-white")}
+      {links.map((link) => {
+        const { Icon, name, path, width } = link;
+        return (
+          <li
+            className={`flex text-neutral-800 ${width} group items-center justify-self-center text-center text-xl`}
+            key={name}
           >
-            Home
-          </span>
-        </Link>
-      </li>
-      <li className="text-neutral flex w-4/12 items-center justify-self-center text-center text-xl">
-        <Link
-          href={"/"}
-          className="group flex  w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 transition duration-500 ease-out hover:bg-neutral-300 hover:text-black dark:text-neutral-300  dark:hover:bg-neutral-800"
-        >
-          <BsFire
-            className={tm(
-              "size-5 flex-shrink-0 text-neutral-400",
-              pathName === "/popular" && "text-white",
-            )}
-          />
-          <span
-            className={tm(
-              "text-neutral-400",
-              pathName === "/popular" && "text-white",
-            )}
-          >
-            Popular
-          </span>
-        </Link>
-      </li>
-      <li className="text-neutral flex w-3/12 items-center justify-self-center text-center text-xl">
-        <Link
-          href={"/"}
-          className="group flex w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 transition duration-500 ease-out hover:bg-neutral-300 hover:text-black dark:text-neutral-300  dark:hover:bg-neutral-800"
-        >
-          <FaClipboardList
-            className={tm(
-              "size-5 text-neutral-400 group-hover:font-bold",
-              pathName === "/list" && "text-white",
-            )}
-          />
-          <span
-            className={tm(
-              "text-neutral-400",
-              pathName === "/list" && "text-white",
-            )}
-          >
-            List
-          </span>
-        </Link>
-      </li>
+            <Link
+              href={path}
+              className={tm(
+                "group flex w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 transition duration-500 ease-out hover:bg-neutral-300 hover:text-black dark:text-neutral-300 dark:hover:bg-neutral-800",
+                path === pathName &&
+                  "border-b-2 border-l-[0.5px] border-r-2 border-t-[0.5px] border-b-amber-600 border-l-neutral-900 border-r-amber-600 border-t-neutral-900 group-hover:border-transparent dark:border-b-violet-800 dark:border-l-neutral-600 dark:border-r-violet-800 dark:border-t-neutral-600",
+              )}
+            >
+              <Icon
+                className={tm(
+                  "size-6 text-neutral-800 group-hover:font-bold dark:text-neutral-400",
+                  path === pathName && "text-black dark:text-white",
+                )}
+              />
+              <span
+                className={tm(
+                  "text-neutral-800 dark:text-neutral-400",
+                  path === pathName && "text-black dark:text-white",
+                )}
+              >
+                {name}
+              </span>
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
--- components/Navbar/VerticalNavList.tsx
+++ components/Navbar/VerticalNavList.tsx
@@ -1,21 +1,21 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { twMerge as tm } from "tailwind-merge";
 
-import { BsFire } from "react-icons/bs";
-import { IoMdHome } from "react-icons/io";
-import { FaClipboardList } from "react-icons/fa";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
 import useHandleOutsideClick from "@/hooks/useHandleOutsideClick";
 import useToggleScroll from "@/hooks/useToggleScroll";
 
 import { VerticalNavProps } from "@/types/navbar-types";
-
+import { links } from "./HorizontalNavList";
+import { usePathname } from "next/navigation";
 const VerticalNavList = ({
   verticalNavVisibility,
   setVerticalNavVisibility,
 }: VerticalNavProps) => {
+  const pathName = usePathname();
   const ref = useHandleOutsideClick(
     verticalNavVisibility,
     setVerticalNavVisibility,
@@ -25,7 +25,7 @@ const VerticalNavList = ({
     <nav
       ref={ref}
       className={tm(
-        "absolute -left-1 -top-[9px] flex h-svh w-64 flex-col items-center justify-start border border-neutral-600 bg-default-white p-5 transition duration-500 ease-linear dark:bg-default-black large-nav:hidden",
+        "absolute -left-1 -top-2 flex h-[105lvh] w-64 flex-col items-center justify-start border border-neutral-600 bg-default-white p-5 transition duration-500 ease-linear dark:bg-default-black large-nav:hidden",
         !verticalNavVisibility && " -translate-x-64",
       )}
     >
@@ -50,40 +50,31 @@ const VerticalNavList = ({
           }}
         />
       </div>
-      <ul className="mt-10 flex w-32 flex-col gap-2 place-self-center">
-        <li className="text-neutral group flex items-center justify-self-center text-center text-xl ring-0">
-          <Link
-            href="/"
-            className="group flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg py-2 text-neutral-600 transition duration-500 ease-in-out hover:text-black dark:text-neutral-300 dark:hover:text-white"
-          >
-            <IoMdHome className="size-6 w-1/5 group-hover:font-bold" />
-            <span className="w-4/5 text-start hover:transition hover:duration-300 hover:ease-in-out">
-              Home
-            </span>
-          </Link>
-        </li>
-        <li className="text-neutral flex items-center justify-self-center text-center text-xl text-neutral-600">
-          <Link
-            href={"/"}
-            className="group flex w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 text-neutral-600 transition duration-500 ease-in-out hover:text-black dark:text-neutral-300 dark:hover:text-white"
-          >
-            <BsFire className="size-5 w-1/5 text-orange-400" />
-            <span className="w-4/5 text-start hover:transition hover:duration-300 hover:ease-in-out">
-              Popular
-            </span>
-          </Link>
-        </li>
-        <li className="text-neutral flex items-center justify-self-center text-center text-xl text-neutral-600">
-          <Link
-            href={"/"}
-            className="group flex w-full cursor-pointer items-center justify-center gap-1 rounded-lg py-2 text-neutral-600 transition duration-500 ease-in-out hover:text-black dark:text-neutral-300 dark:hover:text-white "
-          >
-            <FaClipboardList className="size-5 w-1/5 group-hover:font-bold" />
-            <span className="w-4/5 text-start hover:transition hover:duration-300 hover:ease-in-out">
-              List
-            </span>
-          </Link>
-        </li>
+      <ul className="mt-10 flex w-full flex-col justify-start gap-2 place-self-center">
+        {links.map((link) => {
+          const { name, Icon, path } = link;
+          return (
+            <li
+              className={tm(
+                "text-neutral group flex items-center text-center text-xl",
+                pathName === path && "rounded-lg border-2 border-violet-800",
+              )}
+              key={name}
+            >
+              <Link
+                href={path}
+                className="flex w-full cursor-pointer items-center justify-center py-2 text-neutral-600 transition duration-500 ease-in-out hover:text-black dark:text-neutral-300 dark:hover:text-white"
+              >
+                <div className="flex w-1/2 items-center justify-center gap-2 place-self-center">
+                  <Icon className="size-6 w-2/5 group-hover:font-bold" />
+                  <span className="w-3/5 text-start hover:transition hover:duration-300 hover:ease-in-out">
+                    {name}
+                  </span>
+                </div>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
