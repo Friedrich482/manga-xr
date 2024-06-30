d51dc2f7192f23e30a80dc8b14963e4cd2aa235d
Friedrich482 <friedricht109@gmail.com>
2024-06-30 03:18:11 +0200

    refactor: Icons component
    refactored the Icon components using an array of objects and looping over it with a map instead of hard coding them

--- components/Navbar/Icons.tsx
+++ components/Navbar/Icons.tsx
@@ -1,42 +1,44 @@
 "use client";
 import { MdDarkMode } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
-import Link from "next/link";
 import ThemeMenu from "./ThemeMenu";
 import { useState } from "react";
 import AuthMenu from "./AuthMenu";
 const Icons = () => {
   const [themeMenuVisibility, setThemeMenuVisibility] = useState(false);
   const [authMenuVisibility, setAuthMenuVisibility] = useState(false);
-  const handleThemeButtonClick = () => {
-    setThemeMenuVisibility((prev) => !prev);
-  };
-  const handleAuthButtonClick = () => {
-    setAuthMenuVisibility((prev) => !prev);
-  };
+  const buttons = [
+    {
+      Icon: MdDarkMode,
+      ariaLabel: "Toggle dark mode",
+      handleClick: () => {
+        setThemeMenuVisibility((prev) => !prev);
+      },
+    },
+    {
+      Icon: FaUser,
+      ariaLabel: "User's profile",
+      handleClick: () => {
+        setAuthMenuVisibility((prev) => !prev);
+      },
+    },
+  ];
   return (
     <>
       <div className="flex w-4/12 min-w-24 items-center justify-center gap-1">
-        <button
-          className="rounded-lg p-2 hover:bg-neutral-300 dark:hover:bg-neutral-700"
-          aria-label="Toggle dark mode"
-          onClick={() => {
-            handleThemeButtonClick();
-          }}
-        >
-          <MdDarkMode className="size-6 text-neutral-800 dark:text-neutral-300" />
-        </button>
-        <div>
-          <button
-            className="rounded-lg p-2 hover:bg-neutral-300 dark:hover:bg-neutral-700"
-            aria-label="User's profile"
-            onClick={() => {
-              handleAuthButtonClick();
-            }}
-          >
-            <FaUser className="size-6 text-neutral-800 dark:text-neutral-300" />
-          </button>
-        </div>
+        {buttons.map((button) => {
+          const { Icon, ariaLabel, handleClick } = button;
+          return (
+            <button
+              key={ariaLabel}
+              className="rounded-lg p-2 hover:bg-neutral-300 dark:hover:bg-neutral-700"
+              aria-label={ariaLabel}
+              onClick={handleClick}
+            >
+              <Icon className="size-6 text-neutral-800 dark:text-neutral-300" />
+            </button>
+          );
+        })}
       </div>
       <ThemeMenu
         themeMenuVisibility={themeMenuVisibility}
