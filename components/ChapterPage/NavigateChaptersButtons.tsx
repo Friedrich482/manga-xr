ee32ccf3314bbfadda4bd54aa73eb1ccf97542f8
Friedrich482 <friedricht109@gmail.com>
2024-07-03 19:18:46 +0200

    feat: next./prev buttons
    added fully functional next and prev buttons for a smooth navigation between close chapters, deactivation when there is no next/prev chapter

--- components/ChapterPage/NavigateChaptersButtons.tsx
+++ components/ChapterPage/NavigateChaptersButtons.tsx
@@ -1,6 +1,8 @@
 import { chapterType } from "@/zod-schema/schema";
-import cleanChaptersArray from "@/utils/fetch/cleanChaptersArray";
 import Link from "next/link";
+import getChapterNumber from "@/utils/getChapterNumber";
+import { twMerge as tm } from "tailwind-merge";
+import { GrNext, GrPrevious } from "react-icons/gr";
 
 const NavigateChaptersButtons = ({
   chapterTitleFromUrl,
@@ -11,26 +13,61 @@ const NavigateChaptersButtons = ({
   altTitle: string;
   chapters: chapterType[];
 }) => {
-  const cleanedChapters = cleanChaptersArray(chapters);
-  const chapterNumber = chapterTitleFromUrl.substring(
-    chapterTitleFromUrl.indexOf(" ") + 1,
-  );
-  const chapterPosition = chapters.filter((chapter) =>
-    chapter.chapterTitle.includes(chapterNumber),
-  );
+  const chapterNumber = getChapterNumber(chapterTitleFromUrl);
+  const [currentChapter] = chapters.filter((chapter) => {
+    const { chapterTitle } = chapter;
+    return getChapterNumber(chapterTitle) === chapterNumber;
+  });
+  const currentChapterIndex = chapters.indexOf(currentChapter);
+  const previousChapter =
+    currentChapterIndex < chapters.length - 1
+      ? chapters[currentChapterIndex + 1]
+      : null; // + 1 : because the chapters array is in descending order
+
+  const nextChapter =
+    currentChapterIndex > 0 ? chapters[currentChapterIndex - 1] : null; // -1 : same reason
+
   return (
-    <div className="flex gap-2">
+    <div className="flex flex-wrap gap-2">
       <Link
-        href={`/manga/${altTitle}/chapter-${""}`}
-        className="flex w-28 justify-center rounded-lg border border-neutral-800/50 px-4 py-1 disabled:cursor-not-allowed disabled:text-neutral-500 dark:border-neutral-500/50 dark:text-neutral-300 dark:hover:text-white dark:active:border-neutral-500"
+        href={
+          previousChapter
+            ? `/manga/${altTitle}/chapter-${getChapterNumber(previousChapter.chapterTitle)}`
+            : ""
+        }
+        className={tm(
+          "flex w-36 justify-center gap-1 rounded-lg border border-neutral-800/50 px-4 py-1 disabled:cursor-not-allowed dark:border-neutral-500/50 dark:text-neutral-300 dark:hover:text-white dark:active:border-neutral-500",
+          !previousChapter && "pointer-events-none",
+        )}
       >
-        Previous
+        <GrPrevious
+          className={tm(
+            "size-4 self-center",
+            !previousChapter && "text-neutral-500/40",
+          )}
+        />
+        <span className={tm(!previousChapter && "text-neutral-500/40", "")}>
+          Previous
+        </span>
       </Link>
       <Link
-        href={`/manga/${altTitle}/chapter-${""}`}
-        className="flex w-28 justify-center rounded-lg border border-neutral-800/50 px-4 py-1 disabled:cursor-not-allowed disabled:text-neutral-500 dark:border-neutral-500/50 dark:text-neutral-300 dark:hover:text-white dark:active:border-neutral-500"
+        href={
+          nextChapter
+            ? `/manga/${altTitle}/chapter-${getChapterNumber(nextChapter.chapterTitle)}`
+            : ""
+        }
+        className={tm(
+          "flex w-36 justify-center gap-1 rounded-lg border border-neutral-800/50 px-4 py-1 disabled:cursor-not-allowed dark:border-neutral-500/50 dark:text-neutral-300 dark:hover:text-white dark:active:border-neutral-500",
+          !nextChapter && "pointer-events-none",
+        )}
       >
-        Next
+        <span className={tm(!nextChapter && "text-neutral-500/40")}>Next</span>
+        <GrNext
+          className={tm(
+            "size-4 self-center",
+            !nextChapter && "text-neutral-500/40",
+          )}
+        />
       </Link>
     </div>
   );
