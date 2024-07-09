3a219ad883c89d0dcc4309df5c2c3278cff1178e
Friedrich482 <friedricht109@gmail.com>
2024-07-09 23:26:30 +0200

    refactor: progress bar
    refactored Horizontal and Vertical progress bar in one single component : ProgressBar

--- components/ChapterPage/ProgressBar/ProgressBar.tsx
+++ components/ChapterPage/ProgressBar/ProgressBar.tsx
@@ -1,16 +1,110 @@
 "use client";
+import Link from "next/link";
+import { twMerge as tm } from "tailwind-merge";
 import useStore from "@/hooks/store";
-import HorizontalProgressBar from "./HorizontalProgressBar";
-import VerticalProgressBar from "./VerticalProgressBar";
+import ToggleProgressBarButton from "./ToggleProgressBarButton";
+import { usePathname, useRouter } from "next/navigation";
 
-const ProgressBar = ({ images }: { images: string[] }) => {
-  const { progressBarDirection } = useStore((state) => ({
+const SingleProgressbar = ({ images }: { images: string[] }) => {
+  const {
+    isVisibleImagesArray,
+    progressBarVisibility,
+    setCurrentPageIndex,
+    chapterPagesDisposition,
+    progressBarDirection,
+  } = useStore((state) => ({
+    isVisibleImagesArray: state.isVisibleImagesArray,
+    progressBarVisibility: state.progressBarVisibility,
+    setCurrentPageIndex: state.setCurrentPageIndex,
+    chapterPagesDisposition: state.chapterPagesDisposition,
     progressBarDirection: state.progressBarDirection,
   }));
-  return progressBarDirection === "Horizontal" ? (
-    <HorizontalProgressBar images={images} />
-  ) : (
-    <VerticalProgressBar images={images} />
+
+  const length = images.length;
+  const currentPageIndexVisibility = isVisibleImagesArray.indexOf(true);
+  const router = useRouter();
+  const pathName = usePathname();
+  return (
+    <section
+      className={tm(
+        "group fixed flex  items-end",
+        progressBarDirection === "Horizontal"
+          ? "bottom-2 h-6 w-[97vw] flex-row-reverse"
+          : "top-[4.1rem] h-[85vh] w-[6svw] flex-col-reverse self-end",
+      )}
+    >
+      {progressBarVisibility && (
+        <div
+          className={tm(
+            "flex-shrink-0 rounded-lg bg-transparent group-hover:bg-neutral-500/50",
+            progressBarDirection === "Horizontal"
+              ? "h-1 w-full group-hover:h-3"
+              : "flex h-full w-1 group-hover:w-3",
+          )}
+        >
+          <ul
+            className={tm(
+              "flex h-full w-full",
+              progressBarDirection === "Horizontal"
+                ? "gap-x-[0.5px]"
+                : "flex-col gap-y-[0.5px]",
+            )}
+          >
+            {images.map((image) => {
+              const index = images.indexOf(image);
+              return (
+                <li
+                  key={image}
+                  title={`page ${index + 1}`}
+                  className={tm(
+                    "cursor-pointer bg-transparent",
+                    progressBarDirection === "Horizontal"
+                      ? "h-full border-x border-x-transparent"
+                      : "w-full border-y border-y-transparent",
+                    index !== 0 && progressBarDirection === "Horizontal"
+                      ? "border-l-transparent group-hover:border-l-neutral-500/50"
+                      : "border-t-transparent group-hover:border-t-neutral-500/50",
+                    index !== length - 1 &&
+                      progressBarDirection === "Horizontal"
+                      ? "border-r-transparent group-hover:border-r-neutral-500/50"
+                      : "border-b-transparent group-hover:border-b-neutral-500/50",
+                  )}
+                  style={
+                    progressBarDirection === "Horizontal"
+                      ? { width: `${100 / length}%` }
+                      : { height: `${100 / length}%` }
+                  }
+                >
+                  {chapterPagesDisposition === "Long Strip" ? (
+                    <Link
+                      href={`#page-${index + 1}`}
+                      className={tm(
+                        "flex h-full w-full rounded-lg border border-transparent hover:border-orange-500",
+                        index <= currentPageIndexVisibility &&
+                          "bg-orange-500/50 group-hover:bg-orange-500/70",
+                      )}
+                    />
+                  ) : (
+                    <button
+                      onClick={() => {
+                        router.push(pathName + `#page-${index + 1}`);
+                        setCurrentPageIndex(index);
+                      }}
+                      className={tm(
+                        "flex h-full w-full rounded-lg border border-transparent hover:border-orange-500",
+                        index <= currentPageIndexVisibility &&
+                          "bg-orange-500/50 group-hover:bg-orange-500/70",
+                      )}
+                    />
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+      <ToggleProgressBarButton direction={progressBarDirection} />
+    </section>
   );
 };
-export default ProgressBar;
+export default SingleProgressbar;
