99736e0bbf108926e6d1dde340c3fee4a3f15ea5
Friedrich482 <friedricht109@gmail.com>
2024-07-12 16:35:58 +0200

    fix: layout issue
    fixed the issue of the padding missing between the menu and the last item in the list fro ChapterPagesMenu and ChaptersMenu

--- components/ChapterPage/ChapterPagesMenu.tsx
+++ components/ChapterPage/ChapterPagesMenu.tsx
@@ -62,34 +62,32 @@ const ChapterPagesMenu = ({
         <div
           ref={ref}
           className={tm(
-            "relative z-20 flex h-80 w-44 flex-none items-center justify-start overflow-y-scroll rounded-lg border border-neutral-800 bg-default-white px-2 py-2 dark:bg-default-black",
+            "relative z-20 flex h-80 min-w-44 flex-col overflow-y-scroll rounded-lg border border-neutral-800 bg-default-white px-2 py-2 dark:bg-default-black",
             menuPosition === "bottom of the button"
               ? "top-1"
               : "bottom-[22.5rem]",
           )}
         >
-          <ul className="flex h-full w-full flex-col items-center justify-start gap-[2px]">
-            {images.map((image) => {
-              const pageNumber = images.indexOf(image) + 1;
-              return (
-                <li
-                  key={pageNumber}
-                  className="flex w-full cursor-pointer items-center justify-start rounded-lg py-1 pl-2 hover:bg-neutral-300 dark:hover:bg-neutral-700"
+          {images.map((image) => {
+            const pageNumber = images.indexOf(image) + 1;
+            return (
+              <div
+                key={pageNumber}
+                className="flex w-full cursor-pointer items-center justify-start rounded-lg py-1 pl-2 hover:bg-neutral-300 dark:hover:bg-neutral-700"
+              >
+                <Link
+                  href={`/manga/${altTitle}/${chapterSlug}/#page-${pageNumber}`}
+                  onClick={() => {
+                    setChapterPagesMenuVisibility(false);
+                    setCurrentPageIndex(pageNumber - 1);
+                  }}
+                  className="w-full"
                 >
-                  <Link
-                    href={`/manga/${altTitle}/${chapterSlug}/#page-${pageNumber}`}
-                    onClick={() => {
-                      setChapterPagesMenuVisibility(false);
-                      setCurrentPageIndex(pageNumber - 1);
-                    }}
-                    className="w-full"
-                  >
-                    Page {pageNumber} / {images.length}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+                  Page {pageNumber} / {images.length}
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </div>
     )
--- components/ChapterPage/ChaptersMenu.tsx
+++ components/ChapterPage/ChaptersMenu.tsx
@@ -37,28 +37,26 @@ const ChaptersMenu = ({
         <div
           ref={ref}
           className={tm(
-            "relative z-20 flex h-80 min-w-44 flex-none items-center justify-start overflow-y-scroll rounded-lg border border-neutral-800 bg-default-white px-2 py-2 dark:bg-default-black",
+            "relative z-20 flex h-80 min-w-44 flex-col overflow-y-scroll rounded-lg border border-neutral-800 bg-default-white px-2 py-2 dark:bg-default-black",
             menuPosition === "bottom of the button"
               ? "top-1"
               : "bottom-[22.5rem]",
           )}
         >
-          <ul className="flex h-full w-full flex-col items-center justify-start gap-[2px]">
-            {chapters.map((chapter) => {
-              const { chapterTitle } = chapter;
-              const chapterNumber = getChapterNumber(chapterTitle);
-              return (
-                <li
-                  key={chapterTitle}
-                  className="flex w-full cursor-pointer items-center justify-start rounded-lg py-1 pl-2 hover:bg-neutral-300 dark:hover:bg-neutral-700"
-                >
-                  <Link href={`/manga/${altTitle}/chapter-${chapterNumber}`}>
-                    Chapter {chapterNumber}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+          {chapters.map((chapter) => {
+            const { chapterTitle } = chapter;
+            const chapterNumber = getChapterNumber(chapterTitle);
+            return (
+              <div
+                key={chapterTitle}
+                className="flex w-full cursor-pointer items-center justify-start rounded-lg py-1 pl-2 hover:bg-neutral-300 dark:hover:bg-neutral-700"
+              >
+                <Link href={`/manga/${altTitle}/chapter-${chapterNumber}`}>
+                  Chapter {chapterNumber}
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </div>
     )
