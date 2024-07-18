bb0e0244b903666b866dff0da2b786963b478c8b
Friedrich482 <friedricht109@gmail.com>
2024-07-18 21:15:08 +0200

    fix: chaptersMenu component
    fixed the active state of a chapter in the chaptersMenu component

--- components/ChapterPage/ChaptersDropDown.tsx
+++ components/ChapterPage/ChaptersDropDown.tsx
@@ -3,22 +3,20 @@ import { useRef, useState } from "react";
 import { FaCaretDown } from "react-icons/fa";
 import ChaptersMenu from "./ChaptersMenu";
 import { chapterType } from "@/zod-schema/schema";
-import { useParams } from "next/navigation";
 import useStore from "@/hooks/store";
 
 const ChaptersDropDown = ({ chapters }: { chapters: chapterType[] }) => {
-  const { chapterSlug }: { chapterSlug: string } = useParams();
-  const chapterTitleFromUrl = chapterSlug.replaceAll("-", " ");
   const [chaptersMenuVisibility, setChaptersMenuVisibility] = useState(false);
   const ref = useRef<HTMLButtonElement>(null);
   const { setChaptersButtonPosition } = useStore((state) => ({
     setChaptersButtonPosition: state.setChaptersButtonPosition,
   }));
+  const currentChapterTitle = chapters[0].chapterTitle;
   return (
     <div className="">
       <button
         ref={ref}
-        className="flex items-center gap-x-1 rounded-lg border border-neutral-500/50 px-2 py-1 hover:border-neutral-500 max-options-menu-breakpoint-2:text-base options-menu-breakpoint-2:w-48 options-menu-breakpoint-2:justify-around options-menu-breakpoint-2:gap-x-3"
+        className="flex items-center gap-x-1 rounded-lg border border-neutral-500/50 px-2 py-1 hover:border-neutral-500 max-options-menu-breakpoint-2:text-base options-menu-breakpoint-2:justify-around options-menu-breakpoint-2:gap-x-3"
         onClick={() => {
           setChaptersMenuVisibility((prev) => !prev);
         }}
@@ -30,10 +28,7 @@ const ChaptersDropDown = ({ chapters }: { chapters: chapterType[] }) => {
           }
         }}
       >
-        <div>
-          {chapterTitleFromUrl.charAt(0).toUpperCase() +
-            chapterTitleFromUrl.slice(1)}
-        </div>
+        <div>{currentChapterTitle}</div>
         <div className="h-full">
           <FaCaretDown />
         </div>
@@ -42,6 +37,7 @@ const ChaptersDropDown = ({ chapters }: { chapters: chapterType[] }) => {
         chaptersMenuVisibility={chaptersMenuVisibility}
         setChaptersMenuVisibility={setChaptersMenuVisibility}
         chapters={chapters}
+        currentChapterTitle={currentChapterTitle}
       />
     </div>
   );
