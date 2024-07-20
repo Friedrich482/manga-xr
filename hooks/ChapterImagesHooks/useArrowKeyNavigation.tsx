31aa201bbab3a99be8b47c30a75a8c9578c554e1
Friedrich482 <friedricht109@gmail.com>
2024-07-20 20:32:40 +0200

    feat: navigation with keys
    added the option to navigate with left and right arrow keys when in "Single Page" navigation
    
    needs to consider the reading mode to the key navigation

---
+++ hooks/ChapterImagesHooks/useArrowKeyNavigation.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import useStore from "../store";
+import useUpdateUrlAndScrollToTop from "./useUpdateUrlAndScrollToTop";
+import { usePathname, useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+const arrowKeyNavigation = (
+  event: KeyboardEvent,
+  targetRefs: React.MutableRefObject<HTMLImageElement[]>,
+  images: string[],
+  router: AppRouterInstance,
+  pathName: string,
+) => {
+  const { currentPageIndex, setCurrentPageIndex, readingDirection } =
+    useStore.getState();
+  if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+    if (event.key === "ArrowLeft" && currentPageIndex !== 0) {
+      setCurrentPageIndex(currentPageIndex - 1);
+    } else if (
+      event.key === "ArrowRight" &&
+      currentPageIndex !== images.length - 1
+    ) {
+      setCurrentPageIndex(currentPageIndex + 1);
+    }
+    useUpdateUrlAndScrollToTop(targetRefs, router, pathName);
+  }
+};
+const useArrowKeyNavigation = (
+  targetRefs: React.MutableRefObject<HTMLImageElement[]>,
+  images: string[],
+) => {
+  const { chapterPagesDisposition } = useStore((state) => ({
+    chapterPagesDisposition: state.chapterPagesDisposition,
+  }));
+  const router = useRouter();
+  const pathName = usePathname();
+  useEffect(() => {
+    if (chapterPagesDisposition === "Single Page") {
+      window.addEventListener("keydown", (e) => {
+        arrowKeyNavigation(e, targetRefs, images, router, pathName);
+      });
+
+      return () => {
+        window.removeEventListener("keydown", (e) => {
+          arrowKeyNavigation(e, targetRefs, images, router, pathName);
+        });
+      };
+    }
+  }, [chapterPagesDisposition]);
+};
+
+export default useArrowKeyNavigation;
