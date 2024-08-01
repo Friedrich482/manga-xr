e5079ff590537aed35d506d3d2753db40c4e71d0
Friedrich482 <friedricht109@gmail.com>
2024-08-01 23:30:24 +0200

    chore: env variables
    updated the parsed environment variables from zod

--- env.ts
+++ env.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const envVarSchema = z.object({
   DATABASE_URL: z.string().min(3),
-  AUTH: z.string().min(15),
+  SESSION_SECRET: z.string().min(10),
 });
 
 export default envVarSchema;
