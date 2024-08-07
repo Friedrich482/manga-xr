c8e550e10d101fa54e22d7d27a0ad1adb96a9997
Friedrich482 <friedricht109@gmail.com>
2024-08-08 01:28:46 +0200

    fix: search bar behavior
    fixed the search bar on small screens (design) and changed the color of load spinners for the pages of a manga

--- components/Navbar/SubmitSearchButton.tsx
+++ components/Navbar/SubmitSearchButton.tsx
@@ -3,15 +3,22 @@ import SquaredIconButton from "@/components/lib/SquaredIconButton";
 import { useFormStatus } from "react-dom";
 import { FaSearch } from "react-icons/fa";
 import { ClipLoader } from "react-spinners";
+import { twMerge as tm } from "tailwind-merge";
 
-const LargeSubmitFormButton = () => {
+const SubmitSearchButton = ({
+  className,
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   const { pending } = useFormStatus();
+
   return (
     <SquaredIconButton
       disabled={pending}
       type="submit"
       aria-label="Search button"
-      className="self-center hover:bg-transparent dark:hover:bg-transparent"
+      className={tm(
+        "size-10 self-center rounded-l-none border border-neutral-800/50 dark:border-neutral-600/50",
+        className,
+      )}
     >
       {pending ? (
         <div className="flex size-full items-center justify-center">
@@ -23,4 +30,4 @@ const LargeSubmitFormButton = () => {
     </SquaredIconButton>
   );
 };
-export default LargeSubmitFormButton;
+export default SubmitSearchButton;
--- components/Skeleton/ChapterPage/ChapterImagesSkeleton.tsx
+++ components/Skeleton/ChapterPage/ChapterImagesSkeleton.tsx
@@ -9,10 +9,13 @@ const ChapterImagesSkeleton = () => {
   return (
     <section className="flex w-5/6 flex-col items-center justify-start self-center">
       <div className="flex w-full flex-col gap-44">
-        {list.map((_) => (
-          <div className="flex w-full items-center justify-center" key={_}>
+        {list.map((element) => (
+          <div
+            className="flex w-full items-center justify-center"
+            key={element}
+          >
             <ClipLoader
-              color="#fb923c"
+              color="#b91c1c"
               size={100}
               speedMultiplier={1}
               cssOverride={{ borderWidth: "8px" }}
