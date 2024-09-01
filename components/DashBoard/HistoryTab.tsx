befa7cbf62df5d7e1abe08929ba854dee637a00c
Friedrich482 <friedricht109@gmail.com>
2024-09-01 22:51:00 +0200

    feat: history
    added the history in desc order with an appropriate skeleton

--- components/DashBoard/HistoryTab.tsx
+++ components/DashBoard/HistoryTab.tsx
@@ -2,12 +2,13 @@ import { Suspense } from "react";
 import SectionTitle from "../lib/SectionTitle";
 import History from "./History";
 import PrincipalSection from "../lib/PrincipalSection";
+import HistorySkeleton from "../Skeleton/HistorySkeleton";
 
 const HistoryTab = () => {
   return (
     <PrincipalSection className="w-full">
       <SectionTitle className="w-full">History</SectionTitle>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<HistorySkeleton />}>
         <History />
       </Suspense>
     </PrincipalSection>
