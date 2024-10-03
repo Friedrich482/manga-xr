94a059f55a5edff65019f781fde73af6af85427a
Friedrich482 <friedricht109@gmail.com>
2024-10-04 00:44:51 +0200

    fix: cache
    fixed the cache tags and memoization issues of scrapping functions by wrapping unstable_cache in cache and defining correctly the value of the tags before passing them to unstable_cache

--- utils/fetch/fetchUnitMangaInfo.ts
+++ utils/fetch/fetchUnitMangaInfo.ts
@@ -13,125 +13,128 @@ import initBrowser from "../initBrowser";
 import { unstable_cache } from "next/cache";
 
 let keyTitle = "";
-export const fetchUnitMangaInfo = unstable_cache(
-  cache(async (mangaSlug: string) => {
-    keyTitle = mangaSlug;
-    let browser: Browser;
-    const { title } = getSeasonFromTitle(mangaSlug);
+export const fetchUnitMangaInfo = cache((mangaSlug: string) => {
+  keyTitle = mangaSlug;
+  return unstable_cache(
+    async (mangaSlug: string) => {
+      let browser: Browser;
+      const { title } = getSeasonFromTitle(mangaSlug);
 
-    try {
-      browser = await initBrowser();
-      const page = await browser.newPage();
+      try {
+        browser = await initBrowser();
+        const page = await browser.newPage();
 
-      await page.setViewport({
-        width: 1080,
-        height: 768,
-      });
+        await page.setViewport({
+          width: 1080,
+          height: 768,
+        });
 
-      page.setDefaultNavigationTimeout(0);
-      await page.goto(`${MAIN_URL}/manga/${title}`);
-      const pageTitle = await page.title();
-      if (pageTitle === "404 Page Not Found") {
-        return 404;
-      }
-      const data = await page.$(
-        "div.MainContainer > div.row > div.col-md-12 > div.Box > div.BoxBody > div.row",
-      );
-      let partialData: PartialMangaUnitDataType = {
-        image: "",
-        title: "",
-        author: "",
-        genres: "",
-        releaseDate: "",
-        synopsis: "",
-        latestUpdateDate: "",
-      };
-      if (data) {
-        const image = await data.$eval("div.col-md-3 > img", (el) => el.src);
-        const title = (await data.$eval(
-          "div.col-md-9 > ul > li",
-          (el) => el.textContent,
-        )) as string;
-
-        const listElements = await page.$$(
-          "div.MainContainer > div.row > div.col-md-12 > div.Box > div.BoxBody > div.row > div.col-md-9 > ul > li",
+        page.setDefaultNavigationTimeout(0);
+        await page.goto(`${MAIN_URL}/manga/${title}`);
+        const pageTitle = await page.title();
+        if (pageTitle === "404 Page Not Found") {
+          return 404;
+        }
+        const data = await page.$(
+          "div.MainContainer > div.row > div.col-md-12 > div.Box > div.BoxBody > div.row",
         );
-        if (listElements) {
-          for (const element of listElements) {
-            const potentialData = (await element.evaluate((el) =>
-              el.textContent?.trim(),
-            )) as string;
-            if (potentialData.includes("Author(s):")) {
-              const author = potentialData.substring(
-                potentialData.indexOf("Author(s):") + "Author(s):".length + 2,
-              );
-              partialData = { ...partialData, author };
-            } else if (potentialData.includes("Genre(s):")) {
-              const genres = potentialData.substring(
-                potentialData.indexOf("Genre(s):") + "Genre(s):".length + 2,
-              );
-              partialData = { ...partialData, genres };
-            } else if (potentialData.includes("Released:")) {
-              const releaseDate = potentialData.substring(
-                potentialData.indexOf("Released:") + "Released:".length + 1,
-              );
-              partialData = { ...partialData, releaseDate };
-            } else if (potentialData.includes("Description:")) {
-              const synopsis = potentialData.substring(
-                potentialData.indexOf("Description:") +
-                  "Description:".length +
-                  1,
-              );
-              partialData = { ...partialData, synopsis };
+        let partialData: PartialMangaUnitDataType = {
+          image: "",
+          title: "",
+          author: "",
+          genres: "",
+          releaseDate: "",
+          synopsis: "",
+          latestUpdateDate: "",
+        };
+        if (data) {
+          const image = await data.$eval("div.col-md-3 > img", (el) => el.src);
+          const title = (await data.$eval(
+            "div.col-md-9 > ul > li",
+            (el) => el.textContent,
+          )) as string;
+
+          const listElements = await page.$$(
+            "div.MainContainer > div.row > div.col-md-12 > div.Box > div.BoxBody > div.row > div.col-md-9 > ul > li",
+          );
+          if (listElements) {
+            for (const element of listElements) {
+              const potentialData = (await element.evaluate((el) =>
+                el.textContent?.trim(),
+              )) as string;
+              if (potentialData.includes("Author(s):")) {
+                const author = potentialData.substring(
+                  potentialData.indexOf("Author(s):") + "Author(s):".length + 2,
+                );
+                partialData = { ...partialData, author };
+              } else if (potentialData.includes("Genre(s):")) {
+                const genres = potentialData.substring(
+                  potentialData.indexOf("Genre(s):") + "Genre(s):".length + 2,
+                );
+                partialData = { ...partialData, genres };
+              } else if (potentialData.includes("Released:")) {
+                const releaseDate = potentialData.substring(
+                  potentialData.indexOf("Released:") + "Released:".length + 1,
+                );
+                partialData = { ...partialData, releaseDate };
+              } else if (potentialData.includes("Description:")) {
+                const synopsis = potentialData.substring(
+                  potentialData.indexOf("Description:") +
+                    "Description:".length +
+                    1,
+                );
+                partialData = { ...partialData, synopsis };
+              }
             }
           }
+          partialData = {
+            ...partialData,
+            image,
+            title,
+          };
         }
-        partialData = {
-          ...partialData,
-          image,
-          title,
-        };
-      }
-      const chaptersList = await page.$(
-        "div.MainContainer > div.row > div.col-md-12 > div.Box > div.BoxBody > div.list-group",
-      );
-      let chapters: { chapterTitle: string; chapterReleaseDate: string }[] = [];
-      if (chaptersList) {
-        const latestUpdateDate = (await chaptersList.$eval(
-          "a > span.float-right",
-          (el) => el.textContent,
-        )) as string;
-        //   click on the "show all chapters button if it exists"
-        const expandButtonSelector =
-          "body > div.container.MainContainer > div > div > div > div > div.list-group.top-10.bottom-5.ng-scope > div";
-        const expandButton = await page.$(expandButtonSelector);
-        if (expandButton) {
-          await page.click(expandButtonSelector);
-        }
-        const chaptersLink = await chaptersList.$$("a");
-        for (const chapter of chaptersLink) {
-          const chapterTitle = (await chapter.$eval(
-            "span",
+        const chaptersList = await page.$(
+          "div.MainContainer > div.row > div.col-md-12 > div.Box > div.BoxBody > div.list-group",
+        );
+        let chapters: { chapterTitle: string; chapterReleaseDate: string }[] =
+          [];
+        if (chaptersList) {
+          const latestUpdateDate = (await chaptersList.$eval(
+            "a > span.float-right",
             (el) => el.textContent,
           )) as string;
-          const chapterReleaseDate = (await chapter.$eval(
-            "span.float-right",
-            (el) => el.textContent,
-          )) as string;
-          chapters.push({ chapterTitle, chapterReleaseDate });
+          //   click on the "show all chapters button if it exists"
+          const expandButtonSelector =
+            "body > div.container.MainContainer > div > div > div > div > div.list-group.top-10.bottom-5.ng-scope > div";
+          const expandButton = await page.$(expandButtonSelector);
+          if (expandButton) {
+            await page.click(expandButtonSelector);
+          }
+          const chaptersLink = await chaptersList.$$("a");
+          for (const chapter of chaptersLink) {
+            const chapterTitle = (await chapter.$eval(
+              "span",
+              (el) => el.textContent,
+            )) as string;
+            const chapterReleaseDate = (await chapter.$eval(
+              "span.float-right",
+              (el) => el.textContent,
+            )) as string;
+            chapters.push({ chapterTitle, chapterReleaseDate });
+          }
+          partialData = cleanUpPartialMangaUnitInfo({
+            ...partialData,
+            latestUpdateDate,
+          });
         }
-        partialData = cleanUpPartialMangaUnitInfo({
-          ...partialData,
-          latestUpdateDate,
-        });
+        chapters = cleanUpChaptersArray(chapters);
+        const finalData: MangaUnitDataType = { ...partialData, chapters };
+        return finalData;
+      } catch (error) {
+        console.error(error);
       }
-      chapters = cleanUpChaptersArray(chapters);
-      const finalData: MangaUnitDataType = { ...partialData, chapters };
-      return finalData;
-    } catch (error) {
-      console.error(error);
-    }
-  }),
-  [`${FETCH_UNIT_MANGA_INFO_TAG}:${keyTitle}`],
-  { tags: [`${FETCH_UNIT_MANGA_INFO_TAG}:${keyTitle}`], revalidate: 900 },
-);
+    },
+    [`${FETCH_UNIT_MANGA_INFO_TAG}:${keyTitle}`],
+    { tags: [`${FETCH_UNIT_MANGA_INFO_TAG}:${keyTitle}`], revalidate: 900 },
+  )(mangaSlug);
+});
