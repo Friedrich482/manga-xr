25353f4aec5b4edb9a8565b993acc8a357b4ae77
Friedrich482 <friedricht109@gmail.com>
2024-10-11 00:30:14 +0200

    test: utils
    added tests for the getInitialState function

--- utils/store-utils/getGapOptionValue.ts
+++ utils/store-utils/getGapOptionValue.ts
@@ -2,7 +2,7 @@ import { GapOptionName } from "@/zod-schema/schema";
 import { gapOptions } from "@/lib/constants";
 
 const getGapOptionValue = (name: GapOptionName) => {
-  return gapOptions.find((option) => option.name === name)?.value;
+  return gapOptions.find((option) => option.name === name)!.value;
 };
 
 export default getGapOptionValue;
