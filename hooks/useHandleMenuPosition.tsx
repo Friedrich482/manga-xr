d209239f0c2b9e886c888dfcda89e3c1b63734c1
Friedrich WT <friedricht109@gmail.com>
2024-10-16 01:01:07 +0200

    Create useHandleMenuPosition.tsx

---
+++ hooks/useHandleMenuPosition.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { MenuPosition } from "@/zod-schema/schema";
+
+const useHandleMenuPosition = (buttonPosition: number) => {
+  // define the position of the menu depending of the proximity of the screen boundaries
+  const [menuPosition, setMenuPosition] = useState<MenuPosition | null>(
+    "bottom of the button",
+  );
+
+  useEffect(() => {
+    const viewportHeight = window.innerHeight;
+    setMenuPosition(
+      buttonPosition > viewportHeight / 2
+        ? "top of the button"
+        : "bottom of the button",
+    );
+  }, [buttonPosition]);
+  return menuPosition;
+};
+export default useHandleMenuPosition;
