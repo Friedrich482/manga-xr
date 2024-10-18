ab198b181cf35f7b347eeaf1953e8eca17f82976
Friedrich482 <friedricht109@gmail.com>
2024-10-18 22:45:07 +0200

    test: hooks
    added tests for the useMutateBookmark hook and improved the tests cases for the usBookmark hook

--- tests/hooks/auth/useBookmark.test.tsx
+++ tests/hooks/auth/useBookmark.test.tsx
@@ -1,32 +1,10 @@
 import { HttpResponse, http } from "msw";
 import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
-import { render, waitFor } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { GET_BOOKMARK_SWR_KEY } from "@/lib/cache-keys/swr";
 import { SWRConfig } from "swr";
 import { setupServer } from "msw/node";
 import useBookmark from "@/hooks/auth/useBookmark";
-
-const TestComponent = ({
-  chapterSlug,
-  mangaSlug,
-}: {
-  chapterSlug: string;
-  mangaSlug: string;
-}) => {
-  const { bookmark, isLoading } = useBookmark(chapterSlug, mangaSlug);
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  return bookmark ? (
-    <div>
-      <p>{bookmark.id}</p>
-      <p>{bookmark.mangaName}</p>
-      <p>{bookmark.chapterSlug}</p>
-    </div>
-  ) : (
-    <div>No bookmark</div>
-  );
-};
 
 const handlers = [
   http.get(`${GET_BOOKMARK_SWR_KEY}`, ({ request }) => {
@@ -36,7 +14,7 @@ const handlers = [
     return HttpResponse.json({
       bookmark: {
         mangaName: mangaSlug?.replaceAll("-", " ").split("_")[0],
-        chapterSlug: chapterSlug?.replaceAll("-", " "),
+        chapterSlug,
         id: `${chapterSlug}-${mangaSlug}`,
       },
     });
@@ -53,20 +31,29 @@ describe("useBookmark", () => {
   it("should return the data from the handler", async () => {
     const mangaSlug = "Tower-Of-God_2";
     const chapterSlug = "chapter-4";
-    const { getByText, unmount } = render(
-      <SWRConfig
-        value={{
-          dedupingInterval: 2000,
-        }}
-      >
-        <TestComponent chapterSlug={chapterSlug} mangaSlug={mangaSlug} />
-      </SWRConfig>,
+    const { unmount, result } = renderHook(
+      () => useBookmark(chapterSlug, mangaSlug),
+      {
+        wrapper: ({ children }) => (
+          <SWRConfig
+            value={{
+              dedupingInterval: 2000,
+              fetcher: (url) => fetch(url).then((res) => res.json()),
+            }}
+          >
+            {children}
+          </SWRConfig>
+        ),
+      },
     );
 
     await waitFor(() => {
-      expect(getByText(`${chapterSlug}-${mangaSlug}`)).toBeDefined();
-      expect(getByText("Tower Of God")).toBeDefined();
-      expect(getByText("chapter 4")).toBeDefined();
+      expect(result.current.bookmark).toBeDefined();
+      expect(result.current.bookmark).toStrictEqual({
+        mangaName: "Tower Of God",
+        chapterSlug: "chapter-4",
+        id: "chapter-4-Tower-Of-God_2",
+      });
     });
     unmount();
   });
--- tests/hooks/useMutateBookmark.test.tsx
+++ tests/hooks/useMutateBookmark.test.tsx
@@ -1,5 +1,141 @@
-import { it, expect, describe } from "vitest";
+import { HttpResponse, http } from "msw";
+import { afterAll, beforeAll, describe, expect, it, vi } from "vitest";
+import { renderHook, waitFor } from "@testing-library/react";
+import { GET_BOOKMARK_SWR_KEY } from "@/lib/cache-keys/swr";
+import { SWRConfig } from "swr";
+import { afterEach } from "node:test";
+import { setupServer } from "msw/node";
+import useMutateBookmark from "@/hooks/useMutateBookmark";
+import { useTheme } from "next-themes";
+
+const handlers = [
+  http.get(`${GET_BOOKMARK_SWR_KEY}`, ({ request }) => {
+    const url = new URL(request.url);
+    const chapterSlug = url.searchParams.get("chapterSlug");
+    const mangaSlug = url.searchParams.get("mangaSlug");
+    return HttpResponse.json({
+      bookmark: {
+        mangaName: mangaSlug?.replaceAll("-", " ").split("_")[0],
+        chapterSlug: chapterSlug,
+        id: `${chapterSlug}-${mangaSlug}`,
+      },
+    });
+  }),
+];
+
+const server = setupServer(...handlers);
 
 describe("useMutateBookmark", () => {
-  it("should", () => {});
+  beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+  afterAll(() => server.close());
+  afterEach(() => server.resetHandlers());
+
+  const mangaSlug = "Tower-Of-God_2";
+  const chapterSlug = "chapter-4";
+
+  it("should return the bookmark, the mutate function and default toastOptions (light mode)", async () => {
+    const { result, unmount } = renderHook(
+      () => useMutateBookmark(chapterSlug, mangaSlug),
+      {
+        wrapper: ({ children }) => (
+          <SWRConfig
+            value={{
+              dedupingInterval: 2000,
+              fetcher: (url) => fetch(url).then((res) => res.json()),
+            }}
+          >
+            {children}
+          </SWRConfig>
+        ),
+      },
+    );
+    await waitFor(() => {
+      expect(result.current.bookmark).toBeDefined();
+      expect(result.current.bookmark).toStrictEqual({
+        chapterSlug: "chapter-4",
+        mangaName: "Tower Of God",
+        id: "chapter-4-Tower-Of-God_2",
+      });
+      expect(result.current.toastOptions.style).toStrictEqual({
+        background: "rgb(15,15, 15)",
+        color: "#ffffff",
+      });
+      expect(result.current.mutate).toBeDefined();
+    });
+    unmount();
+  });
+
+  it("should return the bookmark, the mutate function and dark mode toastOptions", async () => {
+    vi.mocked(useTheme).mockReturnValue({
+      ...useTheme(),
+      resolvedTheme: "dark",
+    });
+
+    const { result, unmount } = renderHook(
+      () => useMutateBookmark(chapterSlug, mangaSlug),
+      {
+        wrapper: ({ children }) => (
+          <SWRConfig
+            value={{
+              dedupingInterval: 2000,
+              fetcher: (url) => fetch(url).then((res) => res.json()),
+            }}
+          >
+            {children}
+          </SWRConfig>
+        ),
+      },
+    );
+    await waitFor(() => {
+      expect(result.current.bookmark).toBeDefined();
+      expect(result.current.bookmark).toStrictEqual({
+        chapterSlug: "chapter-4",
+        mangaName: "Tower Of God",
+        id: "chapter-4-Tower-Of-God_2",
+      });
+      expect(result.current.toastOptions.style).toStrictEqual({
+        background: "rgb(247, 247, 247)",
+        color: "#000000",
+      });
+    });
+    unmount();
+  });
+
+  it("should return the bookmark, the mutate function and dark mode toastOptions", async () => {
+    vi.mocked(useTheme).mockReturnValue({
+      ...useTheme(),
+      resolvedTheme: "dark",
+    });
+
+    const { result, unmount } = renderHook(
+      () => useMutateBookmark(chapterSlug, mangaSlug),
+      {
+        wrapper: ({ children }) => (
+          <SWRConfig
+            value={{
+              dedupingInterval: 2000,
+              fetcher: (url) => fetch(url).then((res) => res.json()),
+            }}
+          >
+            {children}
+          </SWRConfig>
+        ),
+      },
+    );
+    await waitFor(() => {
+      expect(result.current.bookmark).toBeDefined();
+      expect(result.current.bookmark).toStrictEqual({
+        chapterSlug: "chapter-4",
+        mangaName: "Tower Of God",
+        id: "chapter-4-Tower-Of-God_2",
+      });
+      expect(result.current.toastOptions.style).toStrictEqual({
+        background: "rgb(247, 247, 247)",
+        color: "#000000",
+      });
+    });
+    expect(result.current.mutate).toBeDefined();
+    expect(result.current.mutate).toBeTypeOf("function");
+    unmount();
+  });
 });
