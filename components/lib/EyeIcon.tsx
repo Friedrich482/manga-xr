15ad7d057a5ae3e9f285d2544419cbf81f34ec8a
Friedrich482 <friedricht109@gmail.com>
2024-10-19 20:41:34 +0200

    test: hooks
    added tests for the useEyeIcon hook

--- components/lib/EyeIcon.tsx
+++ components/lib/EyeIcon.tsx
@@ -1,13 +1,14 @@
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import React from "react";
 import { twMerge as tm } from "tailwind-merge";
 const EyeIcon = ({
   name,
-  className,
   visibility,
+  className,
   toggleVisibility,
-}: {
+  role,
+}: React.HTMLAttributes<SVGSVGElement> & {
   name: string;
-  className?: string;
   visibility: Record<string, boolean>;
   // eslint-disable-next-line no-unused-vars
   toggleVisibility: (name: string) => void;
@@ -15,6 +16,7 @@ const EyeIcon = ({
   const IconComponent = visibility[name] ? FaEye : FaEyeSlash;
   return (
     <IconComponent
+      role={role}
       title={visibility[name] ? "Hide" : "Show"}
       onClick={() => toggleVisibility(name)}
       className={tm("size-5 cursor-pointer", className)}
