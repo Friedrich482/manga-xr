792de51807b88ecccb9d958c2cea117f2f6ddbe6
Friedrich482 <friedricht109@gmail.com>
2024-11-10 03:17:45 +0100

    removed the Skeleton folder

--- components/skeleton/AvatarSkeleton.tsx
+++
@@ -1,6 +0,0 @@
-const AvatarSkeleton = () => {
-  return (
-    <div className="size-8 animate-pulse rounded-full bg-neutral-300 dark:bg-neutral-600"></div>
-  );
-};
-export default AvatarSkeleton;
