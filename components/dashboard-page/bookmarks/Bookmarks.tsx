152794d0aebb0d284dd7d6116969c7d8eb35de5e
Friedrich482 <friedricht109@gmail.com>
2025-10-11 00:24:50 +0200

    chore: dependencies - updated nextjs to v15.5.4 - froze prisma and @prisma/client to v6.5.0 - updated vitest to v3.2.4 - fixed some layout issues in the Bookmarks and MangasInHistory components

--- components/dashboard-page/bookmarks/Bookmarks.tsx
+++ components/dashboard-page/bookmarks/Bookmarks.tsx
@@ -11,25 +11,29 @@ const Bookmarks = async () => {
   }
 
   return (
-    <div className="grid w-full min-w-32 grid-cols-1 gap-x-6 gap-y-20 min-[450px]:grid-cols-2 min-[760px]:grid-cols-3 min-[1200px]:grid-cols-4">
+    <div>
       {bookmarks.length > 0 ? (
-        bookmarks.map(({ chapterSlug, image, mangaName, chapterTitle, id }) => (
-          <div className="group relative" key={chapterSlug}>
-            <MangaElement
-              manga={{
-                lastChapter: chapterTitle,
-                image,
-                title: mangaName,
-                chapterSlug,
-              }}
-              link={`/chapters/${chapterSlug}`}
-            />
-            <DeleteDropDown
-              deleteDataServerAction={deleteBookmarkAction}
-              id={id}
-            />
-          </div>
-        ))
+        <div className="grid w-full min-w-32 grid-cols-1 gap-x-6 gap-y-20 min-[450px]:grid-cols-2 min-[760px]:grid-cols-3 min-[1200px]:grid-cols-4">
+          {bookmarks.map(
+            ({ chapterSlug, image, mangaName, chapterTitle, id }) => (
+              <div className="group relative" key={chapterSlug}>
+                <MangaElement
+                  manga={{
+                    lastChapter: chapterTitle,
+                    image,
+                    title: mangaName,
+                    chapterSlug,
+                  }}
+                  link={`/chapters/${chapterSlug}`}
+                />
+                <DeleteDropDown
+                  deleteDataServerAction={deleteBookmarkAction}
+                  id={id}
+                />
+              </div>
+            ),
+          )}
+        </div>
       ) : (
         <div className="w-full text-center">
           No bookmarks yet. Once you{"'"}ll have created some, they will be
